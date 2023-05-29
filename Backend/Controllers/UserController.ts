5d10dd07d413fbef0be44d9082f6767de7bae037
Turki Al-Qahtani <92539391+Turkialq@users.noreply.github.com>
2023-05-29 10:59:31 +0300

    Registration complete
    User can register with username, email and password and the server will create JWT token and store it in HTTP cookie

--- Backend/Controllers/UserController.ts
+++ Backend/Controllers/UserController.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
+import UserModel from "../Models/UserModel";
+import generateToken from "../Utils/GenerateToken";
+import { userType } from "../Interface/UserTypes";
 
 async function loginUser(req: Request, res: Response, next: NextFunction) {
   console.log("authUser");
@@ -6,8 +9,31 @@ async function loginUser(req: Request, res: Response, next: NextFunction) {
 }
 
 async function registerUser(req: Request, res: Response, next: NextFunction) {
-  console.log("registerUser");
-  next();
+  const { name, email, password }: userType = req.body;
+  const userExists = await UserModel.findOne({ email });
+  if (userExists) {
+    res.status(404);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await UserModel.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user.id);
+
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 }
 
 async function logoutUser(req: Request, res: Response, next: NextFunction) {
---
+++ Backend/Utils/GenerateToken.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+
+//create token inside cookie
+export default function generateToken(res: Response, userID: any) {
+  const token = jwt.sign({ userID }, process.env.ACESS_TOKEN_SECRET!, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+  });
+}
